4fe89328fae345b6ebc2b2651a845e6ae6da38ad
lijin <lijin@dianxinos.com>
2014-06-13 15:46:38 +0800

    对登陆用户增加用户表验证

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,9 +1,36 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import models.User;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.session.SqlSession;
+
+import utils.CommonUtils;
+import utils.SqlSessionFactoryUtls;
+
+import com.qiniu.api.auth.digest.Mac;
+import com.qiniu.api.net.CallRet;
+import com.qiniu.api.rs.RSClient;
+
 public class Security extends Secure.Security{
 	static boolean authenticate(String username, String password) {
-		if("admin".equals(username) && "123".equals(password))
-			return true;
-		return false;
+		if(StringUtils.isBlank(username) || StringUtils.isBlank(password))
+			return false;
+		SqlSession sqlSession = SqlSessionFactoryUtls.getSessionFactory().openSession();
+    	try {
+    		Map<String, Object> map = new HashMap<String, Object>();
+    		map.put("username", username);
+    		map.put("password", password);
+    		User user = (User)sqlSession.selectOne("models.User.selectUser", map);
+    		if(user != null){
+    			return true;
+    		}
+    		return false;
+		}finally{
+			sqlSession.close();
+		}
     }
 }
---
+++ app/models/User.java
@@ -0,0 +1,49 @@
+package models;
+
+public class User {
+	private Long id;
+	private String username;
+	private String password;
+	private String realname;
+	private String email;
+	private int level;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+}
